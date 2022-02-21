ab1bae972a263b0333e3f23c669054ce30ded5bf
Michail_Galichev <mv.galichev@yandex.ru>
2022-02-21 14:13:13 +0300

    Создан блок room-cards

---
+++ blocks/cards/room-cards/room-cards.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.room-card--840').find('.room-card__price')
+  .css(({
+    'margin-left': '73px'
+  }));
+
+  $('.room-card--888').find('.rate__star:last').text('star');
+  
+
+});
+
