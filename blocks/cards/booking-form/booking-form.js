0d173361226b5f39459f9ccdd619bb5c82410589
Michail_Galichev <mv.galichev@yandex.ru>
2022-02-25 13:36:48 +0300

    Рефакторинг кода блока  booking-form

--- blocks/cards/booking-form/booking-form.js
+++ blocks/cards/booking-form/booking-form.js
@@ -1,5 +1,8 @@
 $( function(){
+  $('.booking-form__guests-wrapper')
+  .find('.text-field-dropdown__input')
+  .attr('placeholder', '3 гостя');
 
-
-$('.booking-form__button-wrapper .button__price').html('забронировать');
+  $('.booking-form__button-wrapper .button__price')
+  .html('забронировать');
 });
