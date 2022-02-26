8f5cfb1e5c8f82e44f32bb7eea9ac4d6386e4e81
Michail_Galichev <mv.galichev@yandex.ru>
2022-02-26 12:34:59 +0300

    Добавлен блок calendar в search-form

--- blocks/cards/search-form/search-form.js
+++ blocks/cards/search-form/search-form.js
@@ -1,5 +1,7 @@
 $( function(){
   $('.dropdown-guests-empty__form').css({
-    'display' : 'none'
+    'display' : 'none',
+    'border-radius' : '4px',
+    'border' : '1px solid  rgba(31, 32, 65, 0.25)'
   });
 });
