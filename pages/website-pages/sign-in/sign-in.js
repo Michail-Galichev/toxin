42f8b5dbc645c0e758bd252fefc736184e6e66fe
Michail_Galichev <mv.galichev@yandex.ru>
2022-02-27 13:58:35 +0300

    Рефакторинг кода, подключение страницы registration

---
+++ pages/website-pages/sign-in/sign-in.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.entry-form__password-wrapper .text-field-default__input')
+  .attr('placeholder', 'Пароль');
+
+  $('.sign-in__form-wrapper .button__border')
+  .attr('href', '/__/registration/registration.html');
+});
