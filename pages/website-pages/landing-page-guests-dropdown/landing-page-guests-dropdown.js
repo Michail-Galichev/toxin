ce4b026dcb47550bf776c5d08967896e397e76df
Michail Galichev <mv.galichev@yandex.ru>
2022-05-23 11:36:04 +0300

    Рефакторинг кода для создания адаптивности старниц landing-page, landing-page-guests-dropdown

--- pages/website-pages/landing-page-guests-dropdown/landing-page-guests-dropdown.js
+++ pages/website-pages/landing-page-guests-dropdown/landing-page-guests-dropdown.js
@@ -3,7 +3,19 @@ $(function(){
   .find('.js-dropdown-guests-filled-in__form')
   .css({
     'display' : 'flex'
-  })
+  });
+
+  $('.js-guests-dropdown__search-form--adaptive')
+  .find('.js-dropdown-guests-filled-in__form')
+  .css({
+    'display' : 'flex'
+  });
+
+  $('.js-guests-dropdown__search-form--adaptive')
+  .find('.js-guests-filled-in__input')
+  .css({
+    'border' : '1px solid rgba(31, 32, 65, 0.5)'
+  });
   
   $('.js-guests-dropdown__search-form')
   .find('.js-guests-filled-in__input')
@@ -19,5 +31,26 @@ $(function(){
   .css({
     'margin-top' : '16px'
   });
+
+  function $changeBackground (){ 
+    var $backgrounds = [
+      '/landing-page.1c9b862d.png',
+      '/landing-page2.d9c64f6e.png',
+      '/landing-page3.1beb304b.png'
+      ]
+      var $currentImage = 0;
+      (function $showNextImage() {
+        var $backgroundBlock = $('.js-guests-dropdown__background');
+        $backgroundBlock.css(
+          'background-image', 'url(' + $backgrounds[$currentImage] + ')'
+        );
+        $currentImage += 1
+        if ($currentImage >= $backgrounds.length) {
+          $currentImage = 0
+        }
+        setTimeout($showNextImage, 5000)
+      }())
+   }
+  $changeBackground ();
 
 });
